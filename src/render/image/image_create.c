06aa2e3c5125bbea9600d49aa2601718e1e0d136
bgenia <vascherbatov@mail.ru>
2022-03-13 19:26:42 +0300

    Implement mlx error handling

--- src/render/image/image_create.c
+++ src/render/image/image_create.c
@@ -6,12 +6,16 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 04:10:40 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/12 04:59:10 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/13 19:11:59 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 #include <cub3d/render/color.h>
 #include <cub3d/render/image.h>
+
+#include <libft/io/printf.h>
 
 #include <mlx.h>
 
@@ -22,6 +26,11 @@ t_image
 
 	image = (t_image){.mlx = mlx, .width = width, .height = height};
 	image.mlx_image = mlx_new_image(mlx, width, height);
+	if (!image.mlx_image)
+	{
+		ft_dprintf(STDERR_FILENO, "cub3d: fatal: unable to create mlx image\n");
+		exit(1);
+	}
 	image.data = mlx_get_data_addr(
 			image.mlx_image,
 			&image.bits_per_pixel,
