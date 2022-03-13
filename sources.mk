0b50185280e7d3c733b1ad270e718649dc7ba53a
bgenia <vascherbatov@mail.ru>
2022-03-13 18:43:31 +0300

    Update sources.mk

--- sources.mk
+++ sources.mk
@@ -3,7 +3,7 @@ SRC@src := \
 	src/map/map_push_char.c \
 	src/map/map_print.c \
 	src/map/map_destroy.c \
-	src/map/map_push_endl.c \
 	src/map/map_get.c \
 	src/map/map_push_line.c \
+	src/map/map_add_line.c \
 	src/main.c \
