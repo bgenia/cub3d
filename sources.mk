5eca8a0befc621a1461c789e1d77c333baf469c7
bgenia <vascherbatov@mail.ru>
2022-03-14 01:40:26 +0300

    Update sources.mk

--- sources.mk
+++ sources.mk
@@ -16,9 +16,18 @@ SRC@src := \
 	src/render/graphics/image_fill.c \
 	src/render/graphics/image_fill_pixel.c \
 	src/render/graphics/brush_circle.c \
-	src/render/color/color_multiply_colponents.c \
 	src/render/color/color_create.c \
+	src/render/color/color_parse.c \
 	src/render/color/color_get_component.c \
+	src/render/color/color_multiply_components.c \
+	src/vecmath/vec_rotate.c \
+	src/vecmath/vec_length.c \
+	src/vecmath/vec_cross.c \
+	src/vecmath/vec_normalize.c \
+	src/vecmath/vec_dot.c \
+	src/parser/parse_cub_file.c \
+	src/parser/validate_map_char.c \
+	src/parser/validate_file_extension.c \
 	src/map/map_create.c \
 	src/map/map_push_char.c \
 	src/map/map_print.c \
@@ -26,4 +35,7 @@ SRC@src := \
 	src/map/map_get.c \
 	src/map/map_push_line.c \
 	src/map/map_add_line.c \
+	src/asset_manager/asset_manager_load_assets.c \
+	src/asset_manager/asset_manager_create.c \
+	src/asset_manager/asset_manager_free_assets.c \
 	src/main.c \
