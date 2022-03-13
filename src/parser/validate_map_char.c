1d2cb35f88ab326c2d4d889fa8257f18a1a4d150
bgenia <bgenia@student.21-school.ru>
2022-03-14 01:39:47 +0300

    Rename parse_cub -> paser/parse_cub_file, Refactor parse_cub_file

---
+++ src/parser/validate_map_char.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate_map_char.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 01:13:03 by bgenia            #+#    #+#             */
+/*   Updated: 2022/03/14 01:13:17 by bgenia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+
+#include <libft/string/string.h>
+
+const static char *g_map_chars = " 01NSEW";
+
+bool
+	validate_map_char(char c)
+{
+	if (ft_strchr(g_map_chars, c))
+		return (true);
+	return (false);
+}
