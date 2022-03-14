61d9e6f3f9f78910d0224feff935ab364bdefe72
bgenia <bgenia@student.21-school.ru>
2022-03-14 16:55:22 +0300

    Add vec_range

--- src/vecmath/vec_range.c
+++ src/vecmath/vec_range.c
@@ -1,36 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vecmath.h                                          :+:      :+:    :+:   */
+/*   vec_range.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/13 19:58:42 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/14 16:55:13 by bgenia           ###   ########.fr       */
+/*   Created: 2022/03/14 16:53:31 by bgenia            #+#    #+#             */
+/*   Updated: 2022/03/14 16:54:26 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECMATH_H
-# define VECMATH_H
+#include <cub3d/vecmath.h>
 
-# include <libft/tuples.h>
-
-double
-vec_dot(t_double2 a, t_double2 b);
+#include <libft/tuples.h>
 
 double
-vec_cross(t_double2 a, t_double2 b);
-
-double
-vec_length(t_double2 vec);
-
-double
-vec_range(t_double2 a, t_double2 b);
-
-t_double2
-vec_normalize(t_double2 vec);
-
-t_double2
-vec_rotate(t_double2 vec, double angle);
-
-#endif
+	vec_range(t_double2 a, t_double2 b)
+{
+	return (vec_length(ft_double2(a.x - b.x, a.y - b.y)));
+}
