2160b1c79f00678e7868a460833d01166ca95787
bgenia <vascherbatov@mail.ru>
2022-03-14 18:06:12 +0300

    Rename vec_range -> vec_distance

--- src/vecmath/vec_distance.c
+++ src/vecmath/vec_distance.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 16:53:31 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/14 16:54:26 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/14 18:05:49 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <libft/tuples.h>
 
 double
-	vec_range(t_double2 a, t_double2 b)
+	vec_distance(t_double2 a, t_double2 b)
 {
 	return (vec_length(ft_double2(a.x - b.x, a.y - b.y)));
 }
