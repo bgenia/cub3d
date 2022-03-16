2751c7e9c71758b13f65797d1f452466972c73ec
bgenia <bgenia@student.21-school.ru>
2022-03-16 11:29:37 +0300

    Declare game/renderer, game_render_next_frame

---
+++ src/game/render/game_render_next_frame.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_render_next_frame.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 05:14:22 by bgenia            #+#    #+#             */
+/*   Updated: 2022/03/16 11:26:28 by bgenia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cub3d/game/game.h>
+
+#include <libft/io/printf.h>
+
+void
+	game_render_next_frame(t_game_state *state)
+{
+	(void)state;
+}
