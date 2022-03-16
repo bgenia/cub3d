9b1bf508c21704a0b93503cc38e94775609bdcbe
drohanne <test@com>
2022-03-16 23:42:26 +0300

    add check_map. check hero, border, symbols

--- src/check_map/check_map.c
+++ src/check_map/check_map.c
@@ -6,7 +6,7 @@
 /*   By: drohanne <drohanne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 21:48:13 by drohanne          #+#    #+#             */
-/*   Updated: 2022/03/15 22:42:56 by drohanne         ###   ########.fr       */
+/*   Updated: 2022/03/16 23:40:50 by drohanne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,82 @@
 #include <cub3d/asset_manager.h>
 #include <cub3d/parser.h>
 #include <cub3d/check_map.h>
+
+#include <libft/utils.h>
+#include <libft/io/printf.h>
+#include <libft/string/string.h>
+
 #include <stdio.h>
-#include <stdlib.h>
+
+static bool	validate_map_border(t_map *map, size_t i, size_t j, char c)
+{
+	size_t	save_i;
+	size_t	save_j;
+
+	if (c == '\0')
+		return (true);
+	if (ft_strchr("0NSEW", c))
+	{
+		if (i == 0 || i == map->height - 1 || j == 0 || j == map->width - 1)
+			return (false);
+		save_i = i;
+		while (i >= 0)
+		{
+			if (map_get(map, j, i) == '1')
+			{
+				i = save_i;
+				while (i < map->height)
+				{
+					if (map_get(map, j, i) == '1')
+					{
+						save_j = j;
+						while (j >= 0)
+						{
+							if (map_get(map, j, save_i) == '1')
+							{
+								j = save_j;
+								while (j < map->width)
+								{
+									if (map_get(map, j, save_i) == '1')
+										return (true);
+									j++;
+								}
+								return (false);
+							}
+							j--;
+						}
+					}
+					i++;
+				}
+			}
+			i--;
+		}
+	}
+	return (true);
+}
 
 void	check_map(t_map *map)
 {
 	size_t	i;
 	size_t	j;
+	int		count_heroes;
 
+	count_heroes = 0;
 	i = 0;
 	while (i < map->height)
 	{
 		j = 0;
 		while (j < map->width)
 		{
-			if (!validate_map_char(map_get(map, j, i)))
-				exit(1);
+			if (!validate_map_char(map_get(map, j, i)) || !validate_map_border(map, i, j, map_get(map, j, i)))
+				ft_exitf(STDERR_FILENO, EXIT_FAILURE,
+					"Error\nInvalid character in map\n");
+			if (ft_strchr("NSEW", map_get(map, j, i)) && map_get(map, j, i) != '\0')
+				count_heroes++;
 			j++;
 		}
-		printf("\n");
 		i++;
 	}
+	if (count_heroes != 1)
+		ft_exitf(STDERR_FILENO, EXIT_FAILURE, "Error\nThere must be one hero\n");
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: drohanne <drohanne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 01:20:38 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/15 22:36:55 by drohanne         ###   ########.fr       */
+/*   Updated: 2022/03/16 22:19:58 by drohanne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,24 @@ int	main(int argc, char **argv)
 	t_cub_file	cub_file;
 
 	if (argc != 2)
-		//ft_exitf(STDERR_FILENO, EXIT_FAILURE, "Error\n%s\n", "Where is a map?");
-		;
+		ft_exitf(STDERR_FILENO, EXIT_FAILURE, "Error\n%s\n", "Where is a map?");
 	fd = open(argv[1], __O_DIRECTORY);
 	if (fd > 0)
 	{
 		close(fd);
-		//ft_exitf(STDERR_FILENO, EXIT_FAILURE,
-			//"Error\n%s\n", "Directory, not a file");
-			;
+		ft_exitf(STDERR_FILENO, EXIT_FAILURE,
+			"Error\n%s\n", "Directory, not a file");
 	}
 	cub_file = parse_cub_file(argv[1]);
-	ft_printf("%s\n", cub_file.assets.north_texture_path);
-	//map_print(&cub_file.map);
+	//ft_printf("%s\n", cub_file.assets.north_texture_path);
+	check_map(&cub_file.map);
+	map_print(&cub_file.map);
 	free(cub_file.assets.ceiling_color);
 	free(cub_file.assets.floor_color);
 	free(cub_file.assets.north_texture_path);
 	free(cub_file.assets.south_texture_path);
 	free(cub_file.assets.west_texture_path);
 	free(cub_file.assets.east_texture_path);
-	check_map(&cub_file.map);
 	map_destroy(&cub_file.map);
 	return (0);
 }
