bb31bf49fd29386d3ddc780564887b8c0b67220d
bgenia <vascherbatov@mail.ru>
2022-03-16 19:27:09 +0300

    Make ray_cast return an infinite ray if it didn't hit anything

--- src/render/raycasting/ray_cast.c
+++ src/render/raycasting/ray_cast.c
@@ -6,10 +6,11 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 16:39:46 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/14 21:49:07 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/16 19:24:12 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdbool.h>
 #include <math.h>
 
 #include <cub3d/render/raycasting.h>
@@ -20,17 +21,25 @@
 void
 	ray_cast(t_ray *ray, int iteration_limit, t_ray_hit_predicate predicate)
 {
-	int	i;
+	int		i;
+	bool	hit;
 
 	if (!isfinite(ray->length))
 		return ;
 	i = 0;
+	hit = false;
 	while (iteration_limit < 0 || i < iteration_limit)
 	{
 		if (predicate.predicate(ray->position, predicate.arg))
+		{
+			hit = true;
 			break ;
+		}
 		ray->position = vec_add(ray->position, ray->offset);
 		i++;
 	}
-	ray->length = vec_distance(ray->origin, ray->position);
+	if (hit)
+		ray->length = vec_distance(ray->origin, ray->position);
+	else
+		ray->length = INFINITY;
 }
