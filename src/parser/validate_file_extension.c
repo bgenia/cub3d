747bccc1f827b253c8996854e94fe5e3329bc387
bgenia <bgenia@student.21-school.ru>
2022-03-16 22:37:01 +0300

    Fix validate_file_extension crasing when given a file without extension

--- src/parser/validate_file_extension.c
+++ src/parser/validate_file_extension.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 01:11:59 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/14 01:12:29 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/16 22:36:33 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ bool
 	char	*extension;
 
 	extension = ft_strrchr(file, '.');
-	return (ft_streq(extension, ".cub"));
+	return (extension && ft_streq(extension, ".cub"));
 }
