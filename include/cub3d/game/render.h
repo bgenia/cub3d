41f837decdad75b03ab6f161ff89b91430ef9110
bgenia <bgenia@student.21-school.ru>
2022-03-17 00:05:03 +0300

    Implement game/render

--- include/cub3d/game/render.h
+++ include/cub3d/game/render.h
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 18:49:03 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/15 18:50:02 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/16 23:54:59 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 # define RENDER_H
 
 # include <cub3d/game/game.h>
+# include <cub3d/render/raycasting.h>
+
+t_image
+*pick_texture(t_game_state *state, t_ray *ray);
+
+void
+game_render_column(
+	t_game_state *state,
+	t_ray *ray,
+	int ray_index,
+	int ray_count
+	);
 
 void
 game_render_next_frame(t_game_state *state);
