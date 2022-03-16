04398b02677953c7a9b1bff647433116bd3cfaae
bgenia <vascherbatov@mail.ru>
2022-03-16 11:29:37 +0300

    Declare game/renderer, game_render_next_frame

---
+++ include/cub3d/game/render.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 18:49:03 by bgenia            #+#    #+#             */
+/*   Updated: 2022/03/15 18:50:02 by bgenia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RENDER_H
+# define RENDER_H
+
+# include <cub3d/game/game.h>
+
+void
+game_render_next_frame(t_game_state *state);
+
+#endif
