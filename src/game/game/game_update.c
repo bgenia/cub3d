fb8339b1f4d452f065ab4fc10a40de3d34d2ab06
bgenia <bgenia@student.21-school.ru>
2022-03-16 15:31:56 +0300

    Implement player rotation using q/e/й/у

--- src/game/game/game_update.c
+++ src/game/game/game_update.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 18:25:00 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/16 11:24:56 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/16 15:28:29 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <libft/tuples.h>
 
 #define PLAYER_SPEED 1.75e-5
+#define PLAYER_ANGULAR_SPEED M_PI * 5e-5
 
 static void
 	_update_player_position(t_game_state *state)
@@ -29,15 +30,25 @@ static void
 	straight_movement = state->player.direction;
 	straight_movement = vec_scale(straight_movement,
 			state->player.movement_direction.y * PLAYER_SPEED);
-	side_movement = vec_rotate(state->player.direction, M_PI / 2);
+	side_movement = vec_rotate(state->player.direction, -M_PI / 2);
 	side_movement = vec_scale(side_movement,
 			state->player.movement_direction.x * PLAYER_SPEED);
 	movement = vec_add(straight_movement, side_movement);
 	state->player.position = vec_add(state->player.position, movement);
 }
 
+static void
+	_update_player_orientation(t_game_state *state)
+{
+	state->player.direction = vec_rotate(
+			state->player.direction,
+			PLAYER_ANGULAR_SPEED * state->player.rotation_direction
+			);
+}
+
 void
 	game_update(t_game_state *state)
 {
+	_update_player_orientation(state);
 	_update_player_position(state);
 }
--- src/game/hooks/handle_key_press.c
+++ src/game/hooks/handle_key_press.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 18:09:14 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/16 11:26:40 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/16 15:22:25 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,9 @@ int
 		state->player.movement_direction.x = 1;
 	if (key == XK_a || key == XK_Cyrillic_ef)
 		state->player.movement_direction.x = -1;
+	if (key == XK_e || key == XK_Cyrillic_u)
+		state->player.rotation_direction = 1;
+	if (key == XK_q || key == XK_Cyrillic_i_macron)
+		state->player.rotation_direction = -1;
 	return (0);
 }
