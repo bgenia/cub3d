a6e3d85fd6e1d633523b14fc492c258eabbff0a5
bgenia <vascherbatov@mail.ru>
2022-03-17 18:01:58 +0300

    Mouse contorol prototype (apparently does not work properly on WSL)

--- src/game/hooks/register_hooks.c
+++ src/game/hooks/register_hooks.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 16:11:59 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/16 10:18:29 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/17 17:30:43 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@
 #include <X11/Xlib.h>
 
 void
-	register_hooks(t_game_state *game_state)
+	register_hooks(t_game_state *state)
 {
-	mlx_hook(game_state->display.window.mlx_window, DestroyNotify, None,
-		(void *)handle_exit, game_state);
-	mlx_hook(game_state->display.window.mlx_window, KeyPress, KeyPressMask,
-		(void *)handle_key_press, game_state);
-	mlx_hook(game_state->display.window.mlx_window, KeyRelease, KeyReleaseMask,
-		(void *)handle_key_release, game_state);
-	mlx_loop_hook(game_state->display.mlx,
-		(void *)handle_frame, game_state);
+	mlx_hook(state->display.window.mlx_window, DestroyNotify, None,
+		(void *)handle_exit, state);
+	mlx_hook(state->display.window.mlx_window, KeyPress, KeyPressMask,
+		(void *)handle_key_press, state);
+	mlx_hook(state->display.window.mlx_window, KeyRelease, KeyReleaseMask,
+		(void *)handle_key_release, state);
+	mlx_loop_hook(state->display.mlx,
+		(void *)handle_frame, state);
 }
