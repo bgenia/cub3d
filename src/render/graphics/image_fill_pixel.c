53be94594fce3833f8744daf24b79dd3694c61ed
bgenia <vascherbatov@mail.ru>
2022-03-21 12:46:13 +0300

    Implement image masks

--- src/render/graphics/image_fill_pixel.c
+++ src/render/graphics/image_fill_pixel.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 04:25:00 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/16 17:55:24 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/21 12:45:42 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,19 @@
 void
 	image_fill_pixel(t_image *image, t_color color, t_int2 position)
 {
+	t_color	*mask_pixel;
 	t_color	*pixel;
 
-	if (position.x < 0 || position.x > (int)image->width)
+	if (position.x < 0 || position.x >= (int)image->width)
 		return ;
-	if (position.y < 0 || position.y > (int)image->height)
+	if (position.y < 0 || position.y >= (int)image->height)
 		return ;
+	if (image->mask)
+	{
+		mask_pixel = image_get_pixel(image->mask, position.x, position.y);
+		if (*mask_pixel == 0)
+			return ;
+	}
 	pixel = image_get_pixel(image, position.x, position.y);
 	*pixel = color;
 }
