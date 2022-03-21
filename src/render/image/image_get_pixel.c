e2887ad4d82c3ab5a8029fbc51893b434098be04
bgenia <bgenia@student.21-school.ru>
2022-03-21 12:44:50 +0300

    Fix image_get_pixel being able to get pixels out of the image's bounds

--- src/render/image/image_get_pixel.c
+++ src/render/image/image_get_pixel.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 04:17:59 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/12 05:13:29 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/20 03:29:41 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ t_color
 {
 	size_t	pixel_offset;
 
-	x = ft_clamp(x, 0, image->width);
-	y = ft_clamp(y, 0, image->height);
+	x = ft_clamp(x, 0, image->width - 1);
+	y = ft_clamp(y, 0, image->height - 1);
 	pixel_offset = y * image->line_length + x * (image->bits_per_pixel / 8);
 	return ((t_color *)(image->data + pixel_offset));
 }
