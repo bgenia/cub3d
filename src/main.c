cf3e8cc639c02857cf563c8fb9d3aeac64907f0f
bgenia <vascherbatov@mail.ru>
2022-03-21 23:36:59 +0300

    Update game settings

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 16:57:26 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/21 18:01:59 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/21 23:36:44 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,9 @@ static t_game_settings
 		.window_height = 720,
 		.fov = 90,
 		.column_width = 3,
-		.movement_speed = 3e-2,
-		.rotation_speed = 5e-2,
+		.shading = false,
+		.movement_speed = 1.5,
+		.rotation_speed = 1.5,
 		.shift_multiplier = 3,
 		.sync = true,
 		.minimap = {
