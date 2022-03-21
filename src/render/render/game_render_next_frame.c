a7c13950459f36ad3aebe949edc178fc5f68427f
bgenia <bgenia@student.21-school.ru>
2022-03-21 22:35:19 +0300

    Add fps counter

--- src/render/render/game_render_next_frame.c
+++ src/render/render/game_render_next_frame.c
@@ -6,25 +6,31 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 05:14:22 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/21 16:45:58 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/21 22:34:55 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <math.h>
-#include <stdbool.h>
-
 #include <cub3d/game/game.h>
 #include <cub3d/render/render.h>
-#include <cub3d/vecmath.h>
-#include <cub3d/level/map.h>
-#include <cub3d/raycasting/raycasting.h>
-#include <cub3d/graphics/color.h>
-#include <cub3d/graphics/drawing.h>
 #include <cub3d/render/minimap.h>
 #include <cub3d/render/scene.h>
 
 #include <libft/tuples.h>
 #include <libft/math.h>
+#include <libft/string/format.h>
+
+#include <mlx.h>
+
+static void
+	_render_fps_counter(t_game_state *state)
+{
+	char	fps_string[32];
+
+	ft_format(fps_string, 16, "fps: %f",
+		1e6 / state->display.renderer.frame_delta);
+	mlx_string_put(state->display.mlx, state->display.window.mlx_window,
+		10, 10, 0x00ff00, fps_string);
+}
 
 void
 	game_render_next_frame(t_game_state *state)
@@ -32,4 +38,5 @@ void
 	render_scene(state);
 	render_minimap(state);
 	renderer_render_next_frame(&state->display.renderer);
+	_render_fps_counter(state);
 }
