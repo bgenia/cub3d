ac02a3f4c2e5997a427e9f8d8f22cc3ec06b83f9
bgenia <vascherbatov@mail.ru>
2022-03-22 14:58:03 +0300

    Move game update functions to game/loop

--- include/cub3d/game/loop.h
+++ include/cub3d/game/loop.h
@@ -1,24 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   game_update.c                                      :+:      :+:    :+:   */
+/*   loop.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/15 18:25:00 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/22 14:53:47 by bgenia           ###   ########.fr       */
+/*   Created: 2022/03/22 14:46:49 by bgenia            #+#    #+#             */
+/*   Updated: 2022/03/22 14:48:38 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cub3d/game/settings.h>
-#include <cub3d/game/game.h>
-#include <cub3d/game/loop.h>
+#ifndef LOOP_H
+# define LOOP_H
+
+# include <cub3d/game/game.h>
 
 void
-	game_update(t_game_state *state)
-{
-	if (state->settings.mouse_rotation && BONUS_MOUSE_ROTATION)
-		update_mouse_movement(state);
-	update_player_orientation(state);
-	update_player_position(state);
-}
+update_player_position(t_game_state *state);
+
+void
+update_player_orientation(t_game_state *state);
+
+void
+update_mouse_movement(t_game_state *state);
+
+#endif
