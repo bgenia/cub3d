65cda9076495d4f25cc8312755c3c000a622c20d
bgenia <bgenia@student.21-school.ru>
2022-03-22 14:58:19 +0300

    Make minimap work only in bonus builds

--- src/render/render/game_render_next_frame.c
+++ src/render/render/game_render_next_frame.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 05:14:22 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/22 12:50:56 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/22 14:54:07 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void
 	game_render_next_frame(t_game_state *state)
 {
 	render_scene(state);
-	if (state->settings.minimap.enabled)
+	if (state->settings.minimap.enabled && BONUS_MINIMAP)
 		render_minimap(state);
 	renderer_render_next_frame(&state->display.renderer);
 	if (state->settings.performance_overlay)
